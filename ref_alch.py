120f1abdff16832a3b0b305d0d0639c3b5a23089
Vitalygorkov <vet55omsk@gmail.com>
2021-02-13 17:30:58 +0500

    Fixing sqlalchemy module by tutorial

--- ref_alch.py
+++ ref_alch.py
@@ -1,17 +1,17 @@
 # https://www.tutorialspoint.com/sqlalchemy/sqlalchemy_core_sql_expressions.htm
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+engine = create_engine('sqlite:///college.db', echo = True)
 meta = MetaData()
 
-engine = create_engine('sqlite:///:memory:', echo=True)
-
-
-metadata = MetaData()
-users_table = Table('users', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('name', String),
-    Column('fullname', String),
-    Column('password', String)
+students = Table(
+   'students', meta,
+   Column('id', Integer, primary_key = True),
+   Column('name', String),
+   Column('lastname', String),
 )
+meta.create_all(engine)
 
-metadata.create_all(engine)
+ins = students.insert()
+ins = students.insert().values(name = 'Ravi', lastname = 'Kapoor')
+conn = engine.connect()
+result = conn.execute(ins)

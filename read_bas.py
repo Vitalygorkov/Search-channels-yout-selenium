43bcd5402abd32ae6721bfa02d6a6d748c64675e
Vitalygorkov <vet55omsk@gmail.com>
2021-02-24 22:58:32 +0600

    Added function to retrieve channel list from database. It is necessary to add the number of views in the main video search loop

--- read_bas.py
+++ read_bas.py
@@ -6,7 +6,20 @@ cur = conn.cursor()
 my_result = cur.execute("""SELECT * FROM vidos
 """)
 # get top channels
-for i in my_result:
-    print(i[2])
-    print(i[3])
-    print(i[5])
+def PrintResults():
+    nums = 0
+    for i in my_result:
+        print(i[2])
+        print(i[3])
+        print(i[5])
+        print(nums)
+        nums+=1
+
+PrintResults()
+
+def FindBase(key):
+    conn = sqlite3.connect('bazasearch_invest.db')
+    cur = conn.cursor()
+
+    my_result = cur.execute("""SELECT * FROM vidos WHERE 
+    """)

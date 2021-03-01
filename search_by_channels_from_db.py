c89a3a6c25f64606b4a3a22de53ad0ac2aa2d73c
Vitalygorkov <vet55omsk@gmail.com>
2021-03-01 23:59:16 +0600

    The loop now contains data about the channel, views are written to the list separated by commas with a ":" separator, you need to add a pass only through the channels to the data extraction form, and correct the repetitions in the loop

--- search_by_channels_from_db.py
+++ search_by_channels_from_db.py
@@ -4,6 +4,7 @@ import time
 import random
 import unicodedata
 import re
+import datetime
 options = webdriver.ChromeOptions()
 prefs = {"profile.managed_default_content_settings.images": 2}
 options.add_experimental_option("prefs", prefs)
@@ -19,7 +20,7 @@ cur.execute("""CREATE TABLE IF NOT EXISTS vidos(
    name TEXT,
    name_channel TEXT,
    descr TEXT,
-   prosm INT,
+   prosm TEXT,
    pub TEXT,
    link_chan TEXT,
    link TEXT PRIMARY KEY);
@@ -94,7 +95,7 @@ def get_vids():
         #             # print(c.get_attribute('href'))
         #             link_chan = c.get_attribute('href')
 
-        vids = ('1', author_date, name_channel, vid_description, prosm_int, '0', link_chan, vid_link)
+        # vids = ('1', author_date, name_channel, vid_description, prosm_int, '0', link_chan, vid_link)
 
         vids = ('1', author_date, name_channel, vid_description, prosm_int, '0', link_chan, vid_link)
         # print(vids)
@@ -111,7 +112,7 @@ def get_vids():
 
     driver.close()
 
-get_vids()
+# get_vids()
 
 
 def getChannelSet(db):
@@ -127,6 +128,80 @@ def getChannelSet(db):
     return link_set
 
 # print(getChannelSet('bazasearch_invest.db'))
+
+def getChannelSet2(db):
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    my_result = cur.execute("""SELECT * FROM vidos
+    """)
+    # link_set = set()
+    # for i in my_result:
+    #     # print(i[0])
+    #     link_set.add(i[0]+'/videos')
+    # conn.close()
+    # return link_set
+    return my_result
+
+print(getChannelSet2('bazasearch_invest.db'))
+for video in getChannelSet2('bazasearch_invest.db'):
+    print(video[1]) # name video
+    print(video[2]) # name channel
+    print(video[4]) # number of views
+    print(video[6])  # link to channel
+    print(video[7]) # link to video
+
+    driver.get(video[6]+'/videos')
+    time.sleep(1)
+    len_scroll = 3000
+    for i in range(1, 9):
+        driver.execute_script("window.scrollBy(0,{})".format(len_scroll))
+        len_scroll += 6000
+        time.sleep(1)
+        print('прокрутка')
+    for i in driver.find_elements_by_id('video-title'):
+        # print('ссылка на видео ' + str(i.get_attribute('href')))
+        vid_link = str(i.get_attribute('href'))
+        vid_description = str(i.get_attribute('aria-label'))
+        # print(vid_link + '   ' + vid_description)
+        try:
+            author_date = str(vid_description.split('Автор:', 1)[1]).split(' ', 1)[1].rstrip()
+        except:
+            author_date = "author_date ошибка "
+            # print("author_date ошибка" + str(vid_link))
+        stro = unicodedata.normalize('NFKD', author_date)
+        prosm_text = str(re.findall(r"\w{0}\s{0}\d+\s*\d*\s*\d* просм", stro))
+        prosm_int = re.findall(r'\d+', prosm_text)
+        prosm_list = str(video[4]) + ',' + str(datetime.date.today())+':'+str(prosm_int)
+        try:
+            prosm_int = int(''.join(prosm_int))
+        except:
+            prosm_int = 0
+            print('prosm_int исключение' + str(vid_link))
+        name_channel = video[2]
+        link_chan = video[6]
+        # for b in driver.find_elements_by_id('channel-name'):
+        #     for c in b.find_elements_by_tag_name('a'):
+        #         if c.text != '':
+        #             # print(c.text)
+        #             name_channel = c.text
+        #             # print(c.get_attribute('href'))
+        #             link_chan = c.get_attribute('href')
+
+        # vids = ('1', author_date, name_channel, vid_description, prosm_int, '0', link_chan, vid_link)
+
+        vids = ('1', author_date, name_channel, vid_description, prosm_list, '0', link_chan, vid_link)
+        # print(vids)
+        try:
+            cur.execute("INSERT INTO vidos VALUES(?, ?, ?, ?, ?, ?, ?, ?);", vids)
+            conn.commit()
+        except sqlite3.IntegrityError as err:
+            print(str(err) + 'в ссылке: ' + vid_link)
+            my_result = cur.execute("SELECT * FROM vidos WHERE link=?", (link,))
+            print(str(my_result) + 'это принт')
+            # cur.execute("REPLACE INTO vidos VALUES(?, ?, ?, ?, ?, ?, ?, ?);", vids)
+
+
+
 def getLinkListCount(cuont,db):
     num = 0
     links = []
